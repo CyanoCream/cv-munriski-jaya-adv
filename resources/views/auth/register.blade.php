dac57c0e2f8252e1d9d69159799b07307f83b4e9
anisamelani <anisa.melani.p864@gmail.com>
2022-09-25 15:59:40 +0700

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="container">
+    <br><br><br>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card"><div class="card-body">
                 <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                <br><br>
+                <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
@@ -25,7 +25,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="row m-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
@@ -62,16 +62,58 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-8 offset-md-4">
+                            <center>    
+                                <button type="submit" class="btn-login btn-primary">
                                     {{ __('Register') }}
                                 </button>
-                            </div>
+                            </center>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    </div>
+    </div><br><br><br>
 </div>
+
+<style>
+    .row{
+        margin-top:10px;
+    }
+    .container{
+        background-image: url('https://cutewallpaper.org/28/cool-tech-wallpaper-gifs/network-technology-wallpaper-network-optimization-motion-graphics-design.gif');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+    .container .card-body{
+        background-color:#f4f9ff;
+        border-radius:10px;
+        padding:50px;
+        margin:20px;
+        margin-right:35%;
+        margin-left:35%;
+    }
+    .btn-login{
+        align-items: center;
+        background-color:#012245;
+        padding:15px;
+        width:260px;
+        border:1px solid transparent;
+        margin-top:40px;
+        border-radius:10px;
+        font-size:15px;
+        color:white;
+    }
+    .form-control{
+        padding:15px;
+        width:240px;
+        border-radius:10px;
+        border:1px solid transparent;
+    }
+    .card-header{
+        text-align:center;
+        font-size:20px;
+    }
+</style>
 @endsection
